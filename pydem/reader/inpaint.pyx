6cb1105675c723a971e9af5fcc08d50bcb648fdf
mpu-creare <mpu-creare@users.noreply.github.com>
2017-10-05 14:21:48 -0400

    Adding openpiv attribution to inpaint.pyx file

--- pydem/reader/inpaint.pyx
+++ pydem/reader/inpaint.pyx
@@ -1,17 +1,13 @@
 """
-   Copyright 2015 Creare
+This file originates from the openpiv-python project:
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+https://github.com/alexlib/openpiv-python/blob/cython_safe_installation/openpiv/c_src/lib.pyx
 
-       http://www.apache.org/licenses/LICENSE-2.0
+and is licensed under GPL 3.0, a copy of which can be found here:
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+https://github.com/alexlib/openpiv-python/blob/cython_safe_installation/LICENSE.txt
+
+-----------------------------------
 
 A module for various utilities and helper functions
 
